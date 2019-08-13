fd0c59303420cb78919157866edc2883a3ac2f0d
Carlos Quijano <quijanoo12345@gmail.com>
2019-08-12 19:41:28 -0600

    ya sirve login y un grafico

--- www/php/login.php
+++ www/php/login.php
@@ -1,12 +1,13 @@
 <?php
-header("Access-Control-Allow-Origin:*");
+
 include "db.php";
 if(isset($_POST['login'])){
 $usuario=$_POST['alias'];
 $password=$_POST['password'];
-$login = mysql_num_rows(mysqli_query($conn, "SELECT `alias`, `clave_usuario` FROM `usuarios` WHERE `alias`=$usuario and `clave_usuario`=$password"));
-if ($login !=0)
-    echo "succes";
+$login = mysqli_query($con, "SELECT `alias`, `clave_usuario` FROM `usuarios` WHERE `alias`='".$usuario."' and `clave_usuario`='".$password."'");
+
+if ($login)
+    echo "success";
 else
      echo "error";
 }
