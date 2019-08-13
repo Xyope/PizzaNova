90f867564f104c2cfb095486bf2ed95446be6128
Carlos Quijano <quijanoo12345@gmail.com>
2019-08-12 22:45:04 -0600

    arreglando unas cosas

--- www/php/insert_materia.php
+++ www/php/insert_materia.php
@@ -1,15 +1,40 @@
 <?php
-header("Access-Control-Allow-Origin:*");
- include "db.php";
- if(isset($_POST['insert']))
- {
- $nombre_materia=$_POST['nombre_materia'];
- $descripcion=$_POST['descripcion'];
- $cantidad=$_POST['cantidad'];
- $q=mysqli_query($con,"INSERT INTO materias_primas_app (nombre_materia, descripcion, cantidad) VALUES ('$nombre_materia','$descripcion','$cantidad')");
- if($q)
-  echo "success";
- else
-  echo "error";
- }
- ?>
+if (isset($_SERVER['HTTP_ORIGIN'])) {  
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");  
+    header('Access-Control-Allow-Credentials: true');  
+    header('Access-Control-Max-Age: 86400');   
+}  
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {  
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))  
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");  
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))  
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");  
+}
+include "db.php";
+    $nombre_materia=$_POST['nombre_materia'];
+    $descripcion=$_POST['descripcion'];
+    $cantidad=$_POST['cantidad'];
+    $sql = "INSERT INTO `materias_primas_app` (`nombre_materia`, `descripcion`, `cantidad`) VALUES ('".$nombre_materia.""','".$descripcion.""','".$cantidad."')");
+    //$pro = array();
+    $result = $con->query($sql);
+    if ( $result ) {
+        $res['status'] = 1;
+        $res['message'] = "success";
+       /* while ( $row = $result->fetch_object() ) {
+            array_push( $pro,$row);
+            $res['status'] = 1;
+        }*/
+    }
+    else {
+        $res['status'] = 0;
+        $res['message'] = mysqli_error($con);
+    }
+    //$res['dataset'] = $pro;
+    header( 'Content-type: application/json' );
+    echo json_encode($res);
+
+?>
+ 
